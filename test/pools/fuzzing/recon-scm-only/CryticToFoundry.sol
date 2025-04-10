695c5ed8710cb1ebbd571a530ffe14eae0ebeacd
entreprenerd <alex@entreprenerd.xyz>
2025-04-10 15:55:29 +0200

    chore: debug

--- test/pools/fuzzing/recon-scm-only/CryticToFoundry.sol
+++ test/pools/fuzzing/recon-scm-only/CryticToFoundry.sol
@@ -5,16 +5,11 @@ import {FoundryAsserts} from "@chimera/FoundryAsserts.sol";
 
 import {Test, console2} from "forge-std/Test.sol";
 
-import {PoolId, raw, newPoolId} from "src/common/types/PoolId.sol";
-import {ShareClassId} from "src/common/types/ShareClassId.sol";
-import {AssetId, newAssetId} from "src/common/types/AssetId.sol";
-import {D18, d18} from "src/misc/types/D18.sol";
-import {IShareClassManager} from "src/hub/interfaces/IShareClassManager.sol";
-import {JournalEntry} from "src/common/libraries/JournalEntryLib.sol";
-import {AccountId} from "src/common/types/AccountId.sol";
-
 import {TargetFunctions} from "./TargetFunctions.sol";
 import {Helpers} from "test/pools/fuzzing/recon-pools/utils/Helpers.sol";
+
+import {AssetId} from "src/common/types/AssetId.sol";
+
 
 // forge test --match-contract CryticToFoundry --match-path test/pools/fuzzing/recon-scm-only/CryticToFoundry.sol -vv
 contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
@@ -25,7 +20,45 @@ contract CryticToFoundry is Test, TargetFunctions, FoundryAsserts {
 
 
     
-    function test_request_deposit() public {
-      
+    function test_basic() public {
+      shareClassManager_requestDeposit(123);
+      _switchActor(1);
+      shareClassManager_requestDeposit(123);
+      shareClassManager_approveDeposits(123);
+      _logRequests();
+
+      _switchActor(0);
+      shareClassManager_cancelDepositRequest();
+      _logRequests();
+      // They cannot queue again since they requested cancelling
+    //   shareClassManager_requestDeposit(123);
+    //   _logRequests();
+    }
+
+    uint256 count;
+    function _logRequests() public {
+        console2.log("");
+        console2.log("");
+        console2.log("_logRequests", count++);
+
+        {
+            (uint128 depositRequest, uint32 lastUpdate) = shareClassManager.depositRequest(scId, depositAssetId, bytes32(uint256(uint160(_getActor()))));
+            console2.log("depositRequest", depositRequest);
+        }
+
+        {
+            (uint128 redeemRequest, uint32 lastUpdateRedeem) = shareClassManager.redeemRequest(scId, depositAssetId, bytes32(uint256(uint160(_getActor()))));        
+            console2.log("redeemRequest", redeemRequest);
+        }
+
+        {
+            (bool isCancelling, uint128 queuedDepositRequest) = shareClassManager.queuedDepositRequest(scId, depositAssetId, bytes32(uint256(uint160(_getActor()))));
+            console2.log("queuedDepositRequest", queuedDepositRequest);
+        }
+
+        {
+            (bool isCancelling, uint128 queuedRedeemRequest) = shareClassManager.queuedRedeemRequest(scId, depositAssetId, bytes32(uint256(uint160(_getActor()))));
+            console2.log("queuedRedeemRequest", queuedRedeemRequest);
+        }
     }
 }
