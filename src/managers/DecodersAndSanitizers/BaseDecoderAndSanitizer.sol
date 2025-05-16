b6dc1cf94687d3c89a1fa6cda5c663278ee75889
Jeroen Offerijns <git@o6ns.com>
2025-05-16 16:15:31 +0200

    Add vault decoder

--- src/managers/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol
+++ src/managers/DecodersAndSanitizers/BaseDecoderAndSanitizer.sol
@@ -1,6 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.28;
 
+import {PoolId} from "src/common/types/PoolId.sol";
+import {ShareClassId} from "src/common/types/ShareClassId.sol";
+
 contract BaseDecoderAndSanitizer {
     error FunctionNotImplemented(bytes _calldata);
 
@@ -10,86 +13,24 @@ contract BaseDecoderAndSanitizer {
         addressesFound = abi.encodePacked(spender);
     }
 
-    // @desc deposit into the ERC4626 vault
-    // @tag receiver:address:the address of the receiver of the vault tokens
-    function deposit(uint256, address receiver) external view virtual returns (bytes memory addressesFound) {
-        addressesFound = abi.encodePacked(receiver);
-    }
-
-    // @desc mint tokens from the ERC4626 vault
-    // @tag receiver:address:the address of the receiver of the vault tokens
-    function mint(uint256, address receiver) external pure virtual returns (bytes memory addressesFound) {
-        addressesFound = abi.encodePacked(receiver);
-    }
-
-    // @desc withdraw tokens from the ERC4626 vault
-    // @tag receiver:address:the address of the receiver of the vault tokens
-    // @tag owner:address:the address of the owner of the vault tokens
-    function withdraw(uint256, address receiver, address owner)
+    // @desc deposit into the balance sheet
+    function deposit(PoolId, ShareClassId, address asset, uint256, uint128)
         external
         view
         virtual
         returns (bytes memory addressesFound)
     {
-        addressesFound = abi.encodePacked(receiver, owner);
+        addressesFound = abi.encodePacked(asset);
     }
 
-    // @desc redeem tokens from the ERC4626 vault
-    // @tag receiver:address:the address of the receiver of the vault tokens
-    // @tag owner:address:the address of the owner of the vault tokens
-    function redeem(uint256, address receiver, address owner)
+    // @desc withdraw from the balance sheet
+    function withdraw(PoolId, ShareClassId, address asset, uint256, address receiver, uint128)
         external
         view
         virtual
         returns (bytes memory addressesFound)
     {
-        addressesFound = abi.encodePacked(receiver, owner);
-    }
-
-    // @desc submit deposit request into the ERC7540 vault
-    // @tag controller:address:the address of the controller of the request
-    // @tag owner:address:the address of the owner of the assets
-    function requestDeposit(uint256, address controller, address owner)
-        external
-        view
-        virtual
-        returns (bytes memory addressesFound)
-    {
-        addressesFound = abi.encodePacked(controller, owner);
-    }
-
-    // @desc submit redeem request into the ERC7540 vault
-    // @tag controller:address:the address of the controller of the request
-    // @tag owner:address:the address of the owner of the shares
-    function requestRedeem(uint256, address controller, address owner)
-        external
-        view
-        virtual
-        returns (bytes memory addressesFound)
-    {
-        addressesFound = abi.encodePacked(controller, owner);
-    }
-
-    // @desc cancel deposit request into the ERC7887 vault
-    // @tag controller:address:the address of the controller of the request
-    function cancelDepositRequest(uint256, address controller)
-        external
-        view
-        virtual
-        returns (bytes memory addressesFound)
-    {
-        addressesFound = abi.encodePacked(controller);
-    }
-
-    // @desc cancel redeem request into the ERC7887 vault
-    // @tag controller:address:the address of the controller of the request
-    function cancelRedeemRequest(uint256, address controller)
-        external
-        view
-        virtual
-        returns (bytes memory addressesFound)
-    {
-        addressesFound = abi.encodePacked(controller);
+        addressesFound = abi.encodePacked(asset, receiver);
     }
 
     fallback() external {
